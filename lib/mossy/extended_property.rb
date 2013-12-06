1d705083fa92aa643bdbc550b61e9790b362ff76
mroach <mroach@mroach.com>
2013-12-06 17:07:47 -0500

    Added drop_script, fixed param order

--- lib/mossy/extended_property.rb
+++ lib/mossy/extended_property.rb
@@ -13,15 +13,25 @@ module Mossy
     end
 
     def script
-      params = [@name, @value, @level_0_name, @level_0_type]
+      params = [@name, @value, @level_0_type, @level_0_name]
       if !@level_1_type.nil?
         params << @level_1_type << @level_1_name
       end
       if !@level_2_type.nil?
         params << @level_2_type << @level_2_name
       end
-
       "EXEC sp_addextendedproperty #{params.to_sql_list};"
+    end
+
+    def drop_script
+      params = [@name, @level_0_type, @level_0_name]
+      if !@level_1_type.nil?
+        params << @level_1_type << @level_1_name
+      end
+      if !@level_2_type.nil?
+        params << @level_2_type << @level_2_name
+      end
+      "EXEC sp_dropextendedproperty #{params.to_sql_list}"
     end
   end
 end
